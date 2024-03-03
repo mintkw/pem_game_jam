184fe4d69b00053ec227419a840f5bf462b4550d
Ben Stokes <ben@benstokes.org.uk>
2024-03-03 11:17:29 +0000

    moose

--- filesystem/Filesystem.py
+++ filesystem/Filesystem.py
@@ -1,11 +1,9 @@
-import typing
+class Node:
+    def __init__(self, parent, children):
+        self.__parent = parent
+        self.__children = children
 
 class Filesystem:
-    class Node:
-        def __init__(self, parent, children):
-            self.__parent = parent
-            self.__children = children
-
     def __init__(self):
-        self.__root =
+        self.__root = Node(None, )
         self.__current_working_directory =
