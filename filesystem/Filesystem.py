b012ea2eea6a80b90eb82a6f684228b4f783cec3
cxrala <ameliang123@gmail.com>
2024-03-03 11:12:41 +0000

    an attempt at a filesys

--- filesystem/Filesystem.py
+++ filesystem/Filesystem.py
@@ -1,4 +1,11 @@
+import typing
+
 class Filesystem:
+    class Node:
+        def __init__(self, parent, children):
+            self.__parent = parent
+            self.__children = children
 
     def __init__(self):
+        self.__root =
         self.__current_working_directory =
